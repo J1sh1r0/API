44e6210cbd0d42074e834eb5078fd96468d138bd
JISHIRO <kronosiraxx@gmail.com>
2024-08-29 08:59:58 -0500

    Actualizacion

---
+++ Parcial 1/Peticion/script.js
@@ -0,0 +1,23 @@
+document.getElementById('fetchPosts').addEventListener('click', fetchPosts);
+
+function fetchPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(data => {
+            const postsContainer = document.getElementById('posts');
+            postsContainer.innerHTML = ''; // Limpiar el contenido anterior
+
+            data.forEach(post => {
+                const postElement = document.createElement('div');
+                postElement.classList.add('post');
+
+                postElement.innerHTML = `
+                    <h2>${post.title}</h2>
+                    <p>${post.body}</p>
+                `;
+
+                postsContainer.appendChild(postElement);
+            });
+        })
+        .catch(error => console.error('Error al obtener los posts:', error));
+}
