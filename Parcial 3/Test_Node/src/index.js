475db24b86df478fe229febf331595b639dcb2aa
JISHIRO <kronosiraxx@gmail.com>
2024-11-07 07:24:08 -0600

    Test

---
+++ Parcial 3/Test_Node/src/index.js
@@ -0,0 +1,8 @@
+// src/index.js
+
+const { sumar, restar, multiplicar, dividir } = require('./mathOperations');
+
+console.log("Suma de 2 + 3:", sumar(2, 3));
+console.log("Resta de 5 - 2:", restar(5, 2));
+console.log("Multiplicación de 4 * 3:", multiplicar(4, 3));
+console.log("División de 10 / 2:", dividir(10, 2));
