95bdf48251a37523406e0581b6fb064ec3c808b7
JISHIRO <kronosiraxx@gmail.com>
2024-11-13 09:18:31 -0600

    Actualizacion

---
+++ Parcial 3/Chai/test/mathOperations.test.js
@@ -0,0 +1,14 @@
+import * as chai from 'chai';
+import test from 'node:test';
+import * as areas from "../src/areas.js";
+
+test("Si le mando un 2 debe de dar 4", ()=>{
+    let res = areas.areaCuadrado(2);
+    chai.assert.equal(res,4);    
+    chai.assert.typeOf(res, 'number'); 
+})
+
+test("Si le mando un 2 debe de dar 4", ()=>{
+    let res = areas.areaCuadrado(2);
+    chai.expect(res).to.be.a('number')
+})
